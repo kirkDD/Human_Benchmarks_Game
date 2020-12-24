cd47379accfef542b29d7b226e64294206d42115
leo-u <leol15@uw.edu>
2020-12-23 22:40:24 -0800

    fix php error

--- backend/php/server.php
+++ backend/php/server.php
@@ -1,6 +1,5 @@
-
-<!-- php server to talk to sqlite3 db -->
 <?php
+  // php server to talk to sqlite3 db
   $DB = new SQLite3("../database/game_stats.db");
 
   $INSERT_STAT_STMT = $DB->prepare("INSERT INTO
@@ -8,6 +7,7 @@
 
   // add a row
   function insertStat($game_name, $score) {
+    global $INSERT_STAT_STMT;
     $INSERT_STAT_STMT->reset();
     $INSERT_STAT_STMT->bindValue(":game_name", $game_name);
     $INSERT_STAT_STMT->bindValue(":score", $score);
@@ -21,8 +21,8 @@
   // handle data coming in
   if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = json_decode(file_get_contents('php://input'), true);
-    if (is_null($data) || array_key_exists("game_name", $data)
-      || array_key_exists("score", $data)) {
+    if (is_null($data) || !array_key_exists("game_name", $data)
+      || !array_key_exists("score", $data)) {
       echo "rejected, use format {game_name, score}";
     } else {
       insertStat($data["game_name"], $data["score"]);
