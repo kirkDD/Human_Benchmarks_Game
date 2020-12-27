5f533d40e7525a3b2b07abc4d05baa1569dd1f01
kirkD <kirkdai111@gmail.com>
2020-12-27 13:50:11 -0800

    add reaction speed test

---
+++ frontend/reaction_speed/reaction.js
@@ -0,0 +1,17 @@
+// javascript practice
+var clickArea = document.getElementById("click_area");
+// start the game after user clicks the button
+function startGame() {
+    clickArea.innerHTML = "Wait for green...";
+    clickArea.style.background = "Brown";
+    var randomTime =  Math.random() * 70000 + 30000;
+    setTimeout(turnGreen(), randomTime);
+    clickArea.onclick = redOnclick;
+}
+
+function redOnclick() {
+}
+
+function turnGreen() {
+    clickArea.style.background = "Aquamarine";
+}
