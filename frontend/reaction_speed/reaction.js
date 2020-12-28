777122d05ebbb6bfef6c3f4a7a053698ec80171e
kirkD <kirkdai111@gmail.com>
2020-12-28 12:12:21 -0800

    added clear button

--- frontend/reaction_speed/reaction.js
+++ frontend/reaction_speed/reaction.js
@@ -6,6 +6,7 @@ var endTime;
 // to keep track of average
 var totalScore = 0.0;
 var numTry = 0;
+var timer = null;
 // start the game after user clicks the button
 function startGame() {
     turnedGreen = false;
@@ -14,13 +15,15 @@ function startGame() {
     // turns green after a random time period between 2s and 6s
     var randomTime =  Math.random() * 4000 + 2000;
     var greenTimer = setTimeout(turnGreen, randomTime);
+    timer = greenTimer;
     // change the onclick
-    clickArea.onclick = () => redOnclick(greenTimer);
+    clickArea.onclick = redOnclick;
 }
 
-function redOnclick(timer) {
+function redOnclick() {
     endTime = new Date().getTime();
-    clearTimeout(timer);
+    if (timer != null)
+        clearTimeout(timer);
     // clicked before it turns green
     if (!turnedGreen) {
         clickArea.innerHTML = "Clicked too soon!<br>Click to keep going";
@@ -28,7 +31,7 @@ function redOnclick(timer) {
         totalScore += (endTime - startTime);
         numTry++;
         var text = "" + (endTime - startTime) + " ms<br>Click to keep going";
-        var avg = totalScore / numTry;
+        var avg = (totalScore / numTry).toFixed(2);
         text += "<br><br>Average in " + numTry
         + (numTry < 2? " try: " : " tries: ") + avg + "ms";
         clickArea.innerHTML = text;
@@ -43,4 +46,19 @@ function turnGreen() {
     clickArea.style.background = "Aquamarine";
     clickArea.innerHTML = "Now!!!";
     startTime = new Date().getTime();
+}
+// listener for when clear button is clicked
+function clearFun() {
+    totalScore = 0.0;
+    numTry = 0;
+    if (timer != null)
+        clearTimeout(timer);
+    homeScreen();
+}
+
+function homeScreen() {
+    clickArea.innerHTML = "When the red box turns green, "
+    + "CLICK!<br>Click anywhere to start";
+    clickArea.onclick = startGame;
+    clickArea.style.background = 'rgb(' + 57 + ',' + 184 + ',' + 243 + ')';
 }
