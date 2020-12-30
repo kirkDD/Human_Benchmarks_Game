feab926c2e302e19134a11a8ca3880a25a5ba745
Leo Liao UW <leol15@uw.edu>
2020-12-29 16:34:21 -0800

    query stats

--- backend/php/server.php
+++ backend/php/server.php
@@ -15,9 +15,31 @@
     $INSERT_STAT_STMT->execute();
   }
 
-  // for cors
-  header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Headers: X-Requested-With");
+  $RETRIVE_STAT_STMT = $DB->prepare("SELECT * FROM game_stats WHERE
+    game_name = :game_name ORDER BY server_time DESC LIMIT :num_rows");
+
+  // retrive data for a game
+  // gets the 'score' and 'server_time' fields
+  function getGameStats($game_name, $num_rows=50) {
+    global $RETRIVE_STAT_STMT;
+    $RETRIVE_STAT_STMT->reset();
+    $RETRIVE_STAT_STMT->bindValue(":game_name", $game_name);
+    $RETRIVE_STAT_STMT->bindValue(":num_rows", $num_rows);
+    // construct return array
+    $result = $RETRIVE_STAT_STMT->execute();
+    $rowsArray = [];
+    while ($row = $result->fetchArray()) {
+        $tmp = [];
+        $tmp["score"] = $row["score"];
+        $tmp["server_time"] = $row["server_time"];
+        array_push($rowsArray, $tmp);
+    }
+    return $rowsArray;
+  }
+
+  // for cors?
+  // header("Access-Control-Allow-Origin: *");
+  // header("Access-Control-Allow-Headers: X-Requested-With");
   // handle data coming in
   if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = json_decode(file_get_contents('php://input'), true);
@@ -28,6 +50,13 @@
       insertStat($data["game_name"], $data["score"]);
     }
   } else {
-    echo "unimplemented :(";
+    if (is_null($_GET["game_name"])) {
+      echo "unimplemented :( use get param 'game_name'\n";
+    }
+    if (!is_null($_GET["limit"]) && is_numeric($_GET["limit"])) {
+      echo json_encode(getGameStats($_GET["game_name"], $_GET["limit"]));
+    } else {
+      echo json_encode(getGameStats($_GET["game_name"]));
+    }
   }
 ?>
--- backend/scheme.md
+++ backend/scheme.md
@@ -4,9 +4,13 @@
 
 ### php + sqlite3 solution
 
-- php server receiving POST requests
-  + requests in the form {game_name: -, score: -}
 - sqlite table game_stats has fields
   + game_name
   + score
   + server_time (in seconds)
+- requests to php/server.php
+- POST requests
+  + requests in the form {game_name: -, score: -}
+- GET requests
+  + game_name: name of game
+  + limit: number of rows to get
